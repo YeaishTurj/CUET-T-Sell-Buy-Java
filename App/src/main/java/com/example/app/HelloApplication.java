632228f71b249b4796fa9cfd369b1d2e33f97a76
mohammadzia <gmmzia002@gmail.com>
2024-11-07 12:19:44 +0600

    fix

--- App/src/main/java/com/example/app/HelloApplication.java
+++ App/src/main/java/com/example/app/HelloApplication.java
@@ -15,11 +15,10 @@ public class HelloApplication extends Application {
     // Constants for resource paths and dimensions
     private static final String FONT_PATH = "/fonts/";                // Directory where font files are located
     private static final String CSS_PATH = "/css/styles.css";        // Path to the CSS stylesheet
-<<<<<<< HEAD
-    private static final String FXML_PATH = "item_screen.fxml";            // Path to the FXML layout file
-=======
-    private static final String FXML_PATH = "seller_signin_screen.fxml";            // Path to the FXML layout file
->>>>>>> main
+
+    private static final String FXML_PATH = "welcome_screen.fxml";            // Path to the FXML layout file
+
+
     private static final double WIDTH = 1024;                        // Width of the application window
     private static final double HEIGHT = 768;                       // Height of the application window
 
