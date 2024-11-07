20e8b16252e8cb71c9d68b4084083f9e925f82a9
YeaishTurj <yjturj12104@gmail.com>
2024-11-07 12:13:06 +0600

    test test1

--- App/src/main/java/com/example/app/HelloApplication.java
+++ App/src/main/java/com/example/app/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     // Constants for resource paths and dimensions
     private static final String FONT_PATH = "/fonts/";                // Directory where font files are located
     private static final String CSS_PATH = "/css/styles.css";        // Path to the CSS stylesheet
-    private static final String FXML_PATH = "welcome_screen.fxml";            // Path to the FXML layout file
+    private static final String FXML_PATH = "seller_signin_screen.fxml";            // Path to the FXML layout file
     private static final double WIDTH = 1024;                        // Width of the application window
     private static final double HEIGHT = 768;                       // Height of the application window
 
@@ -50,10 +50,13 @@ public class HelloApplication extends Application {
 
         // Iterate through each font file and load it
         for (String fontFile : fontFiles) {
+            Font font=
             Font.loadFont(
                     Objects.requireNonNull(getClass().getResource(FONT_PATH + fontFile)).toExternalForm(),
                     64 // Size of the font
             );
+            if(font==null) System.out.println("Font not loaded");
+            else System.out.println("Font loaded");
         }
     }
 
